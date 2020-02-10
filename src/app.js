b36f388db85dfe918cd11eb433b5a95d3ca97bbe
ThaTruth24 <thankgodegbo@gmail.com>
2020-02-11 00:17:48 +0100

    test9

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(async (req, res, next) => {
     const accessToken = req.session.token;
     const { userId, exp } = await jwt.verify(
       accessToken,
-      "jgu7657u6r76f76r7"
+      process.env.JWT_SECRET
     );
     // Check if token has expired
     if (exp < Date.now().valueOf() / 1000) {
